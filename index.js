160a2a4a022b25b805a7e913481e8a5da745e249
RICARDO SOLIS <ricardosv46@gmail.com>
2022-04-01 00:50:11 -0500

    update

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const cors = require('cors');
 const routerApi = require('./routes');
 const { checkApiKey } = require('./middlewares/auth.handler');
 
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+  ormErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://www.reactshop.xyz'];
+const whitelist = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'https://www.reactshop.xyz',
+];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
@@ -18,8 +27,8 @@ const options = {
     } else {
       callback(new Error('no permitido'));
     }
-  }
-}
+  },
+};
 app.use(cors(options));
 
 require('./utils/auth');
@@ -38,7 +47,6 @@ app.use(logErrors);
 app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
 
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
